fee9fe11d63fa89391b1821210e11751ee596452
Klsandbox <ibrahim@klsandbox.com>
2016-06-02 19:19:17 +0800

    support-new-referral-id

--- src/Klsandbox/BonusModel/BonusModelServiceProvider.php
+++ src/Klsandbox/BonusModel/BonusModelServiceProvider.php
@@ -52,7 +52,7 @@ class BonusModelServiceProvider extends ServiceProvider
             $pattern = "/(?<!\w)(\s*)@blink\(\s*(.*?)\)/";
 
             return preg_replace($pattern, '$1'
-                . '<?php if($auth->admin || $auth->id == $2->user->id || $auth->id == $2->user->referral_id) {?>' . PHP_EOL
+                . '<?php if($auth->admin || $auth->id == $2->user->id || $auth->id == $2->user->referral_id|| $auth->id == $2->user->new_referral_id) {?>' . PHP_EOL
                 . '<a href="/bonus-management/view/<?php echo $2->id ?>">' . PHP_EOL
                 . '#<?php echo (1024 + $2->id) ?>' . PHP_EOL
                 . '</a>' . PHP_EOL
