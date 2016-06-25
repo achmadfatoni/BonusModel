c92c6aed6e943afb191dd243b86ff7577f5047f6
Klsandbox <ibrahim@klsandbox.com>
2016-06-25 17:10:59 +0800

    strong-type-user

--- src/Klsandbox/BonusModel/Services/BonusManager.php
+++ src/Klsandbox/BonusModel/Services/BonusManager.php
@@ -3,6 +3,7 @@
 namespace Klsandbox\BonusModel\Services;
 
 use App\Models\BonusCategory;
+use App\Models\User;
 use Klsandbox\OrderModel\Models\OrderItem;
 use Carbon\Carbon;
 
@@ -10,7 +11,7 @@ interface BonusManager
 {
     public function resolveBonus(OrderItem $orderItem);
 
-    public function resolveBonusCommandsForOrderItemUserDetails($order_item_id, Carbon $created_at, OrderItem $orderItem, $user, BonusCategory $orderItemBonusCategory);
+    public function resolveBonusCommandsForOrderItemUserDetails($order_item_id, Carbon $created_at, OrderItem $orderItem, User $user, BonusCategory $orderItemBonusCategory);
 
     public function getExpiry(BonusCommand $bonusCommand);
 }
